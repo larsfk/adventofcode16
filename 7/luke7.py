53913fd1b8b7fa487dc0393cacc1081c192186f2
larsfk <larsklingen@gmail.com>
2016-12-10 22:20:26 +0100

    Small changes

--- 7/luke7.py
+++ 7/luke7.py
@@ -1,6 +1,8 @@
 IPS = 0
 SSLs = 0
 
+
+#======== Part 1 =======
 def findIPs(ip):
     global IPS
     abba = 0
@@ -13,15 +15,24 @@ def findIPs(ip):
                 abba += 1
     IPS += abba
 
+
+def isAbba(s):
+    for p in zip(s[:-3], s[1:-2], s[2:-1], s[3:]):
+        if p[0] == p[-1] and p[1] == p[2] and s[0] != s[1]:
+            return True
+    return False
+
+#==== Part 2 =====
+
 def findSSL(ip):
     global SSLs
     supernet = []
     hypernet = []
     for i in range(len(ip)):
         if i % 2 == 0:
-            supernet.extend(returnBAB(ip[i]))
+            supernet.extend(returnListOfBABs(ip[i]))
         else:
-            hypernet.extend(returnBAB(ip[i]))
+            hypernet.extend(returnListOfBABs(ip[i]))
     #print "s", supernet, "h", hypernet
 
     if len(hypernet) > 0 and len(supernet) > 0:
@@ -29,22 +40,16 @@ def findSSL(ip):
             aba = bab[1] + bab[0] + bab[1]
             if aba in hypernet:
                 #print "bab", bab, "aba", aba
-                SSLs +=1    
+                SSLs +=1            
 
-                
-
-def returnBAB(s):
+def returnListOfBABs(s):
     bab = []
     for p in zip(s[:-2], s[1:-1], s[2:]):
         if p[0] == p[-1] and p[0] != p[1]:
             bab.append(p[1]+p[0]+p[1])
     return bab
 
-def isAbba(s):
-    for p in zip(s[:-3], s[1:-2], s[2:-1], s[3:]):
-        if p[0] == p[-1] and p[1] == p[2] and s[0] != s[1]:
-            return True
-    return False
+
 
 
 def main():
